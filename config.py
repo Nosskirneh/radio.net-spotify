74d9afea4803730801e3ec4fa7ac39223b65c42f
Andreas Wigforss <ahenriksson@spotify.com>
2024-05-25 11:01:01 +0200

    Fix outdated config

--- config.py
+++ config.py
@@ -1,28 +1,39 @@
 # Spotify configs
-USERNAME      = 'aspen71449'
-CLIENT_ID     = '2cf7eb83b4ff4d678481a5a2147c4fca'
-CLIENT_SECRET = 'd28c147f07464de6b54bc3cd14df0131'
+USERNAME      = ''
+CLIENT_ID     = ''
+CLIENT_SECRET = ''
 
 # Station configs
-stations = {
-    9126: {
+radio_net_stations = {
+    "antenneclassicrock": {
         "station_name": "ANTENNE BAYERN - Classic Rock Live",
-        "playlist_uri": 'spotify:user:aspen71449:playlist:6IMRUAc8RBz0sLLvyJDy2j',
+        "playlist_uri": 'spotify:playlist:6IMRUAc8RBz0sLLvyJDy2j',
         "limit": 400
     },
-    105096: {
-        "station_name": "Star FM Svenska",
-        "playlist_uri": 'spotify:user:aspen71449:playlist:4Jr3WeBQNvoikBgrYULwDS',
-        "limit": 400
-    },
-    10494: {
+}
+
+radio_play_stations = {
+    "nrs": {
         "station_name": "NRJ Sweden",
-        "playlist_uri": 'spotify:user:aspen71449:playlist:24TvGqDGbVkzmMZbVWsuX3',
+        "playlist_uri": 'spotify:playlist:24TvGqDGbVkzmMZbVWsuX3',
         "limit": 400
     },
-    12293: {
+    "mmg": {
+        "station_name": "Mix Megapol Göteborg",
+        "playlist_uri": 'spotify:playlist:6c4YIthwwnAbQ016Iwl4W2',
+        "limit": 400
+    }
+}
+
+ilikeradio_stations = {
+    "94": {
+        "station_name": "STAR FM",
+        "playlist_uri": 'spotify:playlist:4Jr3WeBQNvoikBgrYULwDS',
+        "limit": 400
+    },
+    "3": {
         "station_name": "RIX FM",
-        "playlist_uri": 'spotify:user:aspen71449:playlist:2pn4YrZ8uJYqXuV1PnJbDb',
+        "playlist_uri": 'spotify:playlist:2pn4YrZ8uJYqXuV1PnJbDb',
         "limit": 400
     }
 }
